2a0bd7cac06b490db81b98ce20c7889576a8b446
Kagiso <143019924+Kagiso-me@users.noreply.github.com>
2025-11-28 21:52:32 +0200

    Create bootstrap.sh

---
+++ K3s Cluster Config/bootstrap.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+echo "=== Installing MetalLB ==="
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.14/config/manifests/metallb-native.yaml
+
+cat <<EOF | kubectl apply -f -
+apiVersion: metallb.io/v1beta1
+kind: IPAddressPool
+metadata:
+  name: default
+  namespace: metallb-system
+spec:
+  addresses:
+  - 10.0.10.110-10.0.10.130
+---
+apiVersion: metallb.io/v1beta1
+kind: L2Advertisement
+metadata:
+  name: default
+  namespace: metallb-system
+spec: {}
+EOF
+
+echo "=== Installing Cert-Manager ==="
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.18.0/cert-manager.yaml
+
+echo "=== Deploying Traefik with custom values ==="
+helm repo add traefik https://traefik.github.io/charts
+helm repo update
+
+# Ensure traefik-values.yaml exists in the same directory
+helm upgrade --install traefik traefik/traefik \
+  --namespace kube-system \
+  --create-namespace \
+  --values traefik-values.yaml
+
+echo "=== Bootstrap complete! ==="
+kubectl get pods -n kube-system
